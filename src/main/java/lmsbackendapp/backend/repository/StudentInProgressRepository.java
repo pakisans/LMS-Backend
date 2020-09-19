36c3278bbc7acd90932a6a2866ae7f3f0050f710
pakisans <pakisakac@gmail.com>
2020-09-19 03:49:06 +0200

    repo+

---
+++ src/main/java/lmsbackendapp/backend/repository/StudentInProgressRepository.java
@@ -0,0 +1,17 @@
+package lmsbackendapp.backend.repository;
+
+import lmsbackendapp.backend.model.StudentInProgress;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface StudentInProgressRepository extends JpaRepository<StudentInProgress,Long> {
+    Optional<StudentInProgress> findByStudyYearAndStudentId(Long studentInProgress,Long studentId);
+    @Query("SELECT sy.id FROM StudentInProgress sy JOIN sy.studyYear.subjectRealization sr "
+            + "WHERE  sr.id = ?1 "
+            + "AND sy.student.regUser.username = ?2")
+    Long getStudentInProgressBySubjectRealizationAndStudent(Long subjectRealizationId, String studentUsername);
+}
