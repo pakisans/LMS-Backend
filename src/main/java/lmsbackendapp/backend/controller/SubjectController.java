ca2081245dc0ef2d0fac37646c727f05ede5347a
pakisans <pakisakac@gmail.com>
2020-09-24 09:57:15 +0200

    Create SubjectController.java

---
+++ src/main/java/lmsbackendapp/backend/controller/SubjectController.java
@@ -0,0 +1,15 @@
+package lmsbackendapp.backend.controller;
+
+import lmsbackendapp.backend.service.SubjectService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins={"http://localhost:4200"})
+@RestController
+@RequestMapping("/subject")
+public class SubjectController {
+    @Autowired
+    SubjectService subjectSrvc;
+}
